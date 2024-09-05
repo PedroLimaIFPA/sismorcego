178f4fd30bcc7ba724153163da6761b85fbb846d
PedrolimaIFPA <pedrolimaifpa@gmail.com>
2024-09-05 00:11:36 -0300

    sismorcego v 1.0.1 transcricao

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import { View, Text, Image } from 'react-native';
+class App extends Component{
+  render(){
+
+    let img = 'https://sujeitoprogramador.com/steve.png'
+
+    return(
+      <View>
+        <Text>
+          Hellow Brasil!
+        </Text>
+        <Image
+        source={{ uri: img}}
+        style={{width:300, height: 300}}
+        />
+      </View>
+    )
+  }
+}
+export default App;
