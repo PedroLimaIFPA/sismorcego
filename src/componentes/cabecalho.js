5d845507ae242cd45f52648ad6e15ddf2e6f080b
PedrolimaIFPA <pedrolimaifpa@gmail.com>
2024-11-13 17:52:50 -0300

    v5.0.2

--- src/componentes/cabecalho.js
+++ src/componentes/cabecalho.js
@@ -1,94 +1,74 @@
-// Cabeçalho.js
+// Cabeçalho.js 
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity, Modal } from 'react-native';
+import { View, StyleSheet, Text, Image, TouchableOpacity, Modal, Button } from 'react-native';
 
 class Cabecalho extends Component {
   constructor(props){
     super(props);
-    this.state={
-      modalVisible:false
+    this.state = {
+      modalVisible: false,
     };
 
     this.abrir_menu = this.abrir_menu.bind(this);
     this.fechar_menu = this.fechar_menu.bind(this);
   }
-abrir_menu(){
-this.setState({modalVisible: true})
-}
 
-fechar_menu(visible){
-  this.setState({modalVisible: false})
+  abrir_menu() {
+    this.setState({ modalVisible: true });
   }
-render() {
-  return (
-    <View style={styles.Header}>
-      <View><TouchableOpacity><Image 
-        source={require('../assets/Logomarca.png')} 
-        style={styles.Img}/>
-      </TouchableOpacity></View>
 
-      <Text style={styles.NomeLogo}>SISMORCEGO</Text>
-      
-      <View><TouchableOpacity onPress={ this.abrir_menu }><Image 
-        source={require('../assets/pedro.png')} 
-        style={styles.Perfil}/> 
-      </TouchableOpacity></View>
+  fechar_menu() {
+    this.setState({ modalVisible: false });
+  }
 
-      <Modal transparent={true} animationType='slide'visible={this.state.modalVisible}>
-        <View style={styles.Menu}>
-
-          <View style={styles.Topo_Menu}>
-            
-            <Text>Menu de configurações</Text>
-            
-
-            
-            <TouchableOpacity onPress={ this.fechar_menu} style={styles.BtnFechar}><Text>X</Text></TouchableOpacity>
-            
-          
-          </View>
-
-          <View style={styles.BtnContainer}>
-          <TouchableOpacity><Text style={styles.TextBtn}>Conta</Text></TouchableOpacity>
-          </View>
-
-          <View style={styles.BtnContainer}>
-          <TouchableOpacity><Text style={styles.TextBtn}>Sobre o projeto</Text></TouchableOpacity>
-          </View>
-
-          <View style={styles.BtnContainer}>
-          <TouchableOpacity><Text style={styles.TextBtn}>Dúvidas</Text></TouchableOpacity>
-          </View>
-
-          <View style={styles.BtnContainer}>
-          <TouchableOpacity><Text style={styles.TextBtn}>Creditos</Text></TouchableOpacity>
-          </View>
-
-          <View style={styles.BtnContainer}>
-          <TouchableOpacity><Text style={styles.TextBtn}>Configurações</Text></TouchableOpacity>
-          </View>
-
-          <View style={styles.BtnContainer}>
-          <TouchableOpacity><Text style={styles.TextBtn}>Sair</Text></TouchableOpacity>
-          </View>
-
+  render() {
+    return (
+      <View style={styles.Header}>
+        <View>
+          <TouchableOpacity>
+            <Image source={require('../assets/Logomarca.png')} style={styles.Img} />
+          </TouchableOpacity>
         </View>
-      </Modal>
 
-    </View>
-  );
-};
+        <Text style={styles.NomeLogo}>SISMORCEGO</Text>
+        
+        <View>
+          <TouchableOpacity onPress={this.abrir_menu}>
+            <Image source={require('../assets/pedro.png')} style={styles.Perfil} />
+          </TouchableOpacity>
+        </View>
+
+        <Modal transparent={true} animationType='slide' visible={this.state.modalVisible}>
+          <View style={styles.modalContainer}>
+            <TouchableOpacity 
+            onPress={this.fechar_menu} 
+            style={{backgroundColor:'red', height:50, width:50, alignItems:'center', justifyContent:'center'}}>
+              <Text style={{ fontSize:25, fontWeight:'bold'}}>X</Text>
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={styles.btnopcoes}><Text style={styles.btnText}>Conta</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.btnopcoes}><Text style={styles.btnText}>Sobre o projeto</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.btnopcoes}><Text style={styles.btnText}>Dúvidas</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.btnopcoes}><Text style={styles.btnText}>Créditos</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.btnopcoes}><Text style={styles.btnText}>Configurações</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.btnopcoes}><Text style={styles.btnText}>Sair</Text></TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
 }
+
 const styles = StyleSheet.create({
   Header: {
     height: 65,
     flexDirection: "row",
-    justifyContent: "space-between", // Alinhando logo e perfil nas extremidades
+    justifyContent: "space-between",
     alignItems: "center",
     borderBottomColor: '#000',
     borderBottomWidth: 2,
     backgroundColor: '#f8f8f8',
-    paddingHorizontal: 10, // Espaçamento nas laterais
+    paddingHorizontal: 10,
   },
   Img: {
     width: 70,
@@ -104,42 +84,31 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 25,
     borderWidth: 3,
-    borderColor:'#20033b',
+    borderColor: '#20033b',
   },
-  Topo_Menu:{
-    flexDirection:'row',
+  modalContainer: {
+    flex: 1,
+    backgroundColor: '#20035b',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
   },
-  Menu:{
-    marginTop:70,
-    backgroundColor: '#f1f123',
-    alignItems:'center',
-    flexDirection:'column',
-    
-    width:280,
-  },
+  btnopcoes: {
+    width: 300,
+    height: 80,
+    backgroundColor: '#f1f1f1', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 1,
+    borderColor:'#1025',
+    borderWidth:0.8
 
-  BtnContainer:{
-    backgroundColor:'#ffff',
-    borderColor:'#3c3c3c',
-    borderWidth:0.2,
-    height:60,
-    width:250,
+  },
+  btnText: {
+    color: '#111',
+    fontSize: 25,
+    fontWeight:'bold',
     alignItems:'flex-end'
   },
-  BtnFechar:{
-    backgroundColor:'red',
-    width: 15,
-    height:15,
-    flexDirection:'colum',
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  TextBtn:{
-    fontSize:30,
-    marginRight:19
-  }
-
 });
-
 
 export default Cabecalho;
